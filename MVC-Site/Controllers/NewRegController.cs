a1750c405813ec90d8ea694e73bc67eaa0ee7986
NADUN-TILAN <tilannadun@gmail.com>
2023-04-22 12:13:15 +0530

    Add project files.

---
+++ MVC-Site/Controllers/NewRegController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Site.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+namespace MVC_Site.Controllers
+{
+    public class NewRegController : Controller
+    {
+        // GET: NewReg
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC-Site/Models/UserClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Site.Models
+{
+	public class UserClass
+	{
+		[Required(ErrorMessage ="Enter Username!")]
+		[Display(Name ="Enter UserName :")]
+		[StringLength(maximumLength:7,MinimumLength =3,ErrorMessage ="Username Length Must Be Max 7 & Min 3")]
+		public string Uname { get; set; }
+
+		[Required(ErrorMessage ="Please Enter The Email Address !")]
+		[Display(Name ="Email Id:")]
+		public string Uemail { get; set; }
+
+		[Required(ErrorMessage ="Enter The Password !")]
+		[Display(Name ="Password :")]
+		[DataType(DataType.Password)]
+		public string Upwd { get; set; }
+
+		[Required(ErrorMessage = "Enter The RePassword !")]
+		[Display(Name = "Re-Password :")]
+		[DataType(DataType.Password)]
+		[Compare("Upwd")]
+		public string Repwd { get; set; }
+
+		[Required(ErrorMessage ="Select The Gender !")]
+		[Display(Name ="Gender :")]
+		public char Gender { get; set; }
+
+		[Required(ErrorMessage ="Upload Profile Image")]
+		[Display(Name ="Profile Image :")]
+		public string Uimg { get; set; }
+	}
+}
